82b8f1a74c11078417538d0080fc22694da2c509
vNenad <vasiljevnenad1@gmail.com>
2023-03-07 22:42:21 +0100

    little bit almost

--- js.js
+++ js.js
@@ -20,54 +20,43 @@ tooltips.forEach((tooltip) => {
 });
 
 const items = document.querySelectorAll(".item");
-const info = document.querySelector(".info");
-const grid = document.querySelector(".items");
-const one = document.querySelector(".one");
-const two = document.querySelector(".two");
-const three = document.querySelector(".three");
-const four = document.querySelector(".four");
-const five = document.querySelector(".five");
-const six = document.querySelector(".six");
-const seven = document.querySelector(".seven");
-const eight = document.querySelector(".eight");
-const nine = document.querySelector(".nine");
-const ten = document.querySelector(".ten");
+const drop = document.querySelectorAll(".drop");
+const imgs = document.querySelectorAll(".flex img");
 
 
-items.forEach((item) => {
-  item.addEventListener("click", () => {
-    if (info.style.display = "none") {
-      info.style.display = "block";
-      grid.classList.add('slide-out')
-    } else { 
-      info.style.display = "none";
-    }
-    
+let activeItem = null;
+let rotatedImg = null;
 
-    if (item.querySelector('div h2').innerHTML === 'HTML/CSS') {
-      info.innerHTML = one.innerHTML;
-      one.classList.add('op')
-    } else if (item.querySelector('div h2').innerHTML === 'JavaScript') {
-      info.innerHTML = two.innerHTML;
-      two.classList.add('op')
-    } else if (item.querySelector('div h2').innerHTML === 'SCSS/SASS') {
-      info.innerHTML = three.innerHTML;
-    } else if (item.querySelector('div h2').innerHTML === 'TailwindCSS') {
-      info.innerHTML = four.innerHTML;
-    } else if (item.querySelector('div h2').innerHTML === 'Bootstrap') {
-      info.innerHTML = five.innerHTML;
-    } else if (item.querySelector('div h2').innerHTML === 'Figma') {
-      info.innerHTML = six.innerHTML;
-    } else if (item.querySelector('div h2').innerHTML === 'Angular') {
-      info.innerHTML = seven.innerHTML;
-    } else if (item.querySelector('div h2').innerHTML === 'React') {
-      info.innerHTML = eight.innerHTML;
-    } else if (item.querySelector('div h2').innerHTML === 'Git') {
-      info.innerHTML = nine.innerHTML;
-    } else if (item.querySelector('div h2').innerHTML === 'TypeScript') {
-      info.innerHTML = ten.innerHTML;
-    }
-  });
-});
+// for (let i = 0; i < items.length; i++) {
+//   items[i].addEventListener("click", () => {
+//     if (activeItem !== null && activeItem !== i) {
+//       drop[activeItem].style.maxHeight = null;
+//       if (rotatedImg !== null) {
+//         imgs[rotatedImg].style.transform = "rotateX(0)";
+//       }
+//     }
 
+//     if (drop[i].style.maxHeight) {
+//       drop[i].style.maxHeight = null;
+//       activeItem = null;
+//       if (rotatedImg === i) {
+
+//         imgs[i].style.transform = "rotateX(0)";
+//         rotatedImg = null;
+//       } else {
+
+//         imgs[i].style.transform = "rotate(90deg)";
+//         rotatedImg = i;
+//       }
+//     } else {
+//       drop[i].style.maxHeight = drop[i].scrollHeight + "px";
+//       activeItem = i;
+//       if (rotatedImg !== null) {
+//         imgs[rotatedImg].style.transform = "rotateX(0)";
+//       }
+//       imgs[i].style.transform = "rotate(90deg)";
+//       rotatedImg = i;
+//     }
+//   });
+// }
 
