55acfc0c9bba269037e82438d902d345f24c6542
vNenad <vasiljevnenad1@gmail.com>
2023-03-08 13:54:42 +0100

    tablet version finished

--- js.js
+++ js.js
@@ -20,52 +20,67 @@ tooltips.forEach((tooltip) => {
   });
 });
 
-const items = document.querySelectorAll(".item");
-const drop = document.querySelectorAll(".drop");
-const imgs = document.querySelectorAll(".flex img");
+const items = document.querySelectorAll(".information");
+const hidden = document.querySelectorAll(".hidden");
+const imgs = document.querySelectorAll(".info img");
 
-
-let activeItem = null;
-let rotatedImg = null;
 
 window.addEventListener('load', () => {
   home.classList.add('active');
   tooltips[0].style.visibility = "visible";
 });
 
-console.log(home)
+console.log(imgs)
+
+let activeItem = null;
+let rotatedImg = null;
 
 
-// for (let i = 0; i < items.length; i++) {
-//   items[i].addEventListener("click", () => {
-//     if (activeItem !== null && activeItem !== i) {
-//       drop[activeItem].style.maxHeight = null;
-//       if (rotatedImg !== null) {
-//         imgs[rotatedImg].style.transform = "rotateX(0)";
-//       }
-//     }
+for (let i = 0; i < items.length; i++) {
+  items[i].addEventListener("click", () => {
+    if (activeItem !== null && activeItem !== i) {
+      hidden[activeItem].style.maxHeight = null;
+      if (rotatedImg !== null) {
+        imgs[rotatedImg].style.transform = "rotateX(0)";
+      }
+    }
 
-//     if (drop[i].style.maxHeight) {
-//       drop[i].style.maxHeight = null;
-//       activeItem = null;
-//       if (rotatedImg === i) {
+    if (hidden[i].style.maxHeight) {
+      hidden[i].style.maxHeight = null;
+      activeItem = null;
+      if (rotatedImg === i) {
 
-//         imgs[i].style.transform = "rotateX(0)";
-//         rotatedImg = null;
-//       } else {
+        imgs[i].style.transform = "rotateX(0)";
+        rotatedImg = null;
+      } else {
 
-//         imgs[i].style.transform = "rotate(90deg)";
-//         rotatedImg = i;
-//       }
-//     } else {
-//       drop[i].style.maxHeight = drop[i].scrollHeight + "px";
-//       activeItem = i;
-//       if (rotatedImg !== null) {
-//         imgs[rotatedImg].style.transform = "rotateX(0)";
-//       }
-//       imgs[i].style.transform = "rotate(90deg)";
-//       rotatedImg = i;
-//     }
-//   });
+        imgs[i].style.transform = "rotate(90deg)";
+        rotatedImg = i;
+      }
+    } else {
+      hidden[i].style.maxHeight = hidden[i].scrollHeight + "px";
+      activeItem = i;
+      if (rotatedImg !== null) {
+        imgs[rotatedImg].style.transform = "rotateX(0)";
+      }
+      imgs[i].style.transform = "rotate(90deg)";
+      rotatedImg = i;
+    }
+  });
+}
+
+// :root {
+//   --lightGray: #d8d4d4;
+//   --darkGray: #656262;
+//   --midGray: #535151;
+//   --darkestGray: #272626;
+//   --light: #d68b12;
+//   --whiteGray: #d8d4d4;
 // }
 
+
+const root = document.documentElement;
+
+
+// root.style.setProperty('--lightGray', '#272626');
+// root.style.setProperty('--darkGray', '#d8d4d4');
